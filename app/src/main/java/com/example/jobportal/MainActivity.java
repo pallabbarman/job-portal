dca70f66eb31a1c97a32ad49f87ac8fea109a267
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-05-29 17:03:10 +0600

    registration with firebase

--- app/src/main/java/com/example/jobportal/MainActivity.java
+++ app/src/main/java/com/example/jobportal/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.jobportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+
+    private Button btnLogin;
+    private Button btnRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginFunction();
+    }
+
+    private  void LoginFunction(){
+        email = findViewById(R.id.email_login);
+        password=findViewById(R.id.login_password);
+
+        btnLogin=findViewById(R.id.btn_login);
+        btnRegistration=findViewById(R.id.btn_reg);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jobportal/RegistrationActivity.java
+++ app/src/main/java/com/example/jobportal/RegistrationActivity.java
@@ -1,14 +1,82 @@
 package com.example.jobportal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private EditText emailReg;
+    private EditText passReg;
+
+    private Button btnReg;
+    private Button btnLogin;
+
+    // Firebase Auth
+    private FirebaseAuth mAuth;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        mAuth = FirebaseAuth.getInstance();
+        Registration();
+    }
+
+    private void Registration(){
+
+        emailReg=findViewById(R.id.email_registration);
+        passReg=findViewById(R.id.registration_password);
+
+        btnReg=findViewById(R.id.btn_registration);
+        btnLogin=findViewById(R.id.btn_login);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email=emailReg.getText().toString().trim();
+                String pass=passReg.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    emailReg.setError("Required field");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(pass)){
+                    passReg.setError("Required field");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Successful",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        }
+                    }
+                });
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
     }
 }
